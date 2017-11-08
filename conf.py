d9d662cc04db26652cddfe8a555c948c9ca9d0c7
Keisuke Fujii <fujiisoup@gmail.com>
2017-11-08 19:32:08 +0900

    Change to more formal expression.

--- conf.py
+++ conf.py
@@ -196,7 +196,7 @@ latex_docclass = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'Pythonistas.tex', 'You\'re one of Pythonistas from today',
-     'Keisuke Fujii', 'howto'),
+     'Keisuke Fujii', 'manual'),
 ]
 
 
